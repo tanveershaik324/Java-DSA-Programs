f1ea69c28524db7a16ec43817a7dd17f24127552
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-02 15:30:59 +0530

    Create MergeTwoArrays.java

---
+++ Day-4-Array/MergeTwoArrays.java
@@ -0,0 +1,30 @@
+public class MergeTwoArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            merged[k++] = arr2[j++];
+        }
+
+        System.out.print("Merged Array: ");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+    }
+}
