26a4768d10f585895f6b5b670c5d650f257384e3
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-02 15:30:15 +0530

    Create MissingNumber.java

---
+++ Day-4-Array/MissingNumber.java
@@ -0,0 +1,16 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+        int n = 5;
+
+        int totalSum = n * (n + 1) / 2;
+        int arrSum = 0;
+
+        for (int num : arr) {
+            arrSum += num;
+        }
+
+        int missingNumber = totalSum - arrSum;
+        System.out.println("Missing Number = " + missingNumber);
+    }
+}
