b624d9a64f01799b3eb68d98f621c15494e93d98
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-03 18:35:58 +0530

    Create LeadersInArray.java

---
+++ Day-5-Arrays/LeadersInArray.java
@@ -0,0 +1,15 @@
+public class LeadersInArray {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        int max = arr[arr.length - 1];
+
+        System.out.print(max + " ");
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                System.out.print(max + " ");
+            }
+        }
+    }
+}
