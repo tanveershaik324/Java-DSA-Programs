5417c5f12833978e9693803cc18e83eba4345fc8
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-08 16:13:30 +0530

    Create CharacterFrequency.java

---
+++ Day-8-Strings/CharacterFrequency.java
@@ -0,0 +1,16 @@
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char) i + " : " + freq[i]);
+            }
+        }
+    }
+}
