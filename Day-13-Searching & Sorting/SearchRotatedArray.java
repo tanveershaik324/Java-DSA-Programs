ee02108c314c9a8dba0248b2f62c8862baec01d9
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-12 19:49:53 +0530

    Create SearchRotatedArray.java

---
+++ Day-13-Searching & Sorting/SearchRotatedArray.java
@@ -0,0 +1,32 @@
+public class SearchRotatedArray {
+
+    public static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target < arr[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (target > arr[mid] && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println("Element found at index: " + search(arr, target));
+    }
+}
