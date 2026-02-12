17d6af50b2b6bc4af0c9decf5d1840635d50aaa8
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-12 19:47:32 +0530

    Create BinarySearch.java

---
+++ Day-12-Searching & Sorting/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int key = 7;
+
+        int result = binarySearch(arr, key);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
