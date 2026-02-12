c451a499c51553bf9e771ae0f48b48f57365f894
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-12 19:42:44 +0530

    Create SelectionSort.java

---
+++ Day-11-Searching & sorting/SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
