5405f848ec5c34260df686c7e9f41cfc373a5de7
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-17 11:06:07 +0530

    Create MinInRotatedArray.java

---
+++ Day-14-Searching & Sorting/MinInRotatedArray.java
@@ -0,0 +1,22 @@
+public class MinInRotatedArray {
+
+    public static int findMin(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println("Minimum Element: " + findMin(arr));
+    }
+}
