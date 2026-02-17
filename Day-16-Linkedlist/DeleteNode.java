07324ad9fc213bac2f40a2f75f826baa2dfdce0b
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-17 11:16:42 +0530

    Create DeleteNode.java

---
+++ Day-16-Linkedlist/DeleteNode.java
@@ -0,0 +1,55 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class DeleteNode {
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void delete(int key) {
+        Node temp = head, prev = null;
+
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) return;
+
+        prev.next = temp.next;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DeleteNode list = new DeleteNode();
+        list.insert(30);
+        list.insert(20);
+        list.insert(10);
+
+        list.delete(20);
+        list.display();
+    }
+}
