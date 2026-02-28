57e1663841dfc437657cdf414b09ed5b91a7c892
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-28 20:24:13 +0530

    Create AddTwoNumbers.java

---
+++ Day-19-LinkedList/AddTwoNumbers.java
@@ -0,0 +1,46 @@
+class AddTwoNumbers {
+    static Node add(Node l1, Node l2) {
+        Node dummy = new Node(0);
+        Node curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.data;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.data;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new Node(sum % 10);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    static void print(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node l1 = new Node(2);
+        l1.next = new Node(4);
+        l1.next.next = new Node(3);
+
+        Node l2 = new Node(5);
+        l2.next = new Node(6);
+        l2.next.next = new Node(4);
+
+        Node result = add(l1, l2);
+        print(result);
+    }
+}
