a405ecd4293cc2f1e90b15edb237e4cd5cef572c
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-28 20:40:24 +0530

    Create LargestRectangle.java

---
+++ Day-23-StackAndQueue/LargestRectangle.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class LargestRectangle {
+    public static void main(String[] args) {
+        int heights[] = {2, 1, 5, 6, 2, 3};
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int i = 0;
+
+        while (i < heights.length) {
+            if (stack.isEmpty() || heights[stack.peek()] <= heights[i]) {
+                stack.push(i++);
+            } else {
+                int top = stack.pop();
+                int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
+            maxArea = Math.max(maxArea, area);
+        }
+
+        System.out.println("Max Area: " + maxArea);
+    }
+}
