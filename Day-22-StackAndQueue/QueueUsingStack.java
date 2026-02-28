99aa51a4795438d7e8777e1622be858dcc88a87d
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-28 20:36:52 +0530

    Create QueueUsingStack.java

---
+++ Day-22-StackAndQueue/QueueUsingStack.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class QueueUsingStack {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue Empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStack q = new QueueUsingStack();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+    }
+}
