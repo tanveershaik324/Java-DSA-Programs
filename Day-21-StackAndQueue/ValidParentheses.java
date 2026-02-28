40dfacf72c54d6fe967fbf94d020126a9d0141e8
tanveershaik324 <tanveershaik324@gmail.com>
2026-02-28 20:33:41 +0530

    Create ValidParentheses.java

---
+++ Day-21-StackAndQueue/ValidParentheses.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class ValidParentheses {
+    static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{[()]}";
+        System.out.println(isValid(s));
+    }
+}
